31f80b75f4975c9a84e163daa3fbf14a2a04c6cc
Max Krishka-Pachal <60788817+maxkrishkapachal@users.noreply.github.com>
2022-11-18 16:08:11 -0700

    login/signup page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<link rel="stylesheet" href="nicepage.css" media="screen">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo file_get_contents("login.html");
+        ?>
+    </body>
+</html>
