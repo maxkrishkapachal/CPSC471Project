2a0262262dfb92baea83e18fc71df64eaf857f36
OWOjustnobody <114700917+OWOjustnobody@users.noreply.github.com>
2022-11-26 20:59:07 -0700

    Create header.php

---
+++ media/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+    <title><?php echo $title; ?></title> 
+    <style>
+/* Add a black background color to the top navigation */
+.header {
+    background-color: grey;
+    width: 100%;
+    HEIGHT:5%;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.header a {
+  float: right;
+  color: white;
+  text-align: center;
+  background-color: grey;
+  height:100%;
+  width: 10%;
+  text-decoration: none;
+  font-size: 17px;
+  
+}
+.header a:hover {
+  background-color: lightgrey;
+  color: black;
+}
+
+.header-title{
+  float: left;
+  color: #ffffff;
+  text-align: center;
+  background-color: grey;
+  height:100%;
+  width: 10%;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+
+</style>
+</head>
+<body>
+  <div class="header">
+    <div class="header-title">
+     Media Shelf </div>
+    <a href="main page.php">Main page</a>
+    <a href="media.php">Media</a>
+    <a href="search.php"> Search</a>
+</div>
+</div>
