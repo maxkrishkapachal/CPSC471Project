f6d4958acb6123b05775e92393e9f7eef289231a
Room <108599236+codeongaku25@users.noreply.github.com>
2022-11-29 23:17:29 -0700

    rough idea

--- website/member/blockMember.php
+++ website/member/blockMember.php
@@ -1,1 +1,37 @@
+<?php
+    session_start();
 
+    include("../gen/connect.php");
+    include('../gen/functions.php');
+
+    if(isset($_REQUEST['block'])){
+      DELETE 
+      FROM can_interact as i
+      WHERE i.member_username1 = 'username1'
+            AND i.member_username2 = 'username2'
+      ";
+    }
+    
+?> 
+/*
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <link rel="stylesheet" href="../gen/main.css" media="screen">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <div class="form-wrap">
+            <div class="tabs-content">
+                <div class="active">
+                    <form class="block-member" action="" method="">
+
+                        <input name="block member" type="submit" class="button" value="">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+*/
