d47c1d3910d54bee38cd32e147e09ab4272ea44c
Max Krishka-Pachal <60788817+maxkrishkapachal@users.noreply.github.com>
2022-12-06 16:10:43 -0700

    edit now works

--- comment/editComment.php
+++ comment/editComment.php
@@ -4,11 +4,13 @@
     include("../gen/connect.php");
     include('../gen/functions.php');
 
+    $user_commentID = NULL;
+
     //$user_commentID = $_SESSION['comment-instance'];
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
       $user_commentID = $_GET['comment'];
     }
-
+    
     $user_data = getUserData($conn);
 
     checkTable($conn, 'COMMENT');
@@ -29,14 +31,15 @@
 
     if(isset($_POST['save'])){
         $content = convertQuotes($_POST['content'], "SYMBOLS");
-
+        $user_commentID = $_REQUEST['comment_ID'];
+        
         if(!empty($content)){
             $comment_edit = "
                 UPDATE COMMENT
                 SET content = '$content'
                 WHERE commentID = '$user_commentID'
             ";
-                
+            
             mysqli_query($conn, $comment_edit);
             
             header("Location: ../media/media.php?id=".$user_comment['mediaID']."");
@@ -64,6 +67,7 @@
         <div class="tabs-content">
           <div class="active">
             <form class="edit-comment-form" action="" method="post">
+              <input hidden name="comment_ID" value='<?php echo $user_commentID ?>'>
               <input name="content" type="text" class="input" id="content" autocomplete="off" value="<?php echo $content ?>">
     
               <input name="save" type="submit" class="button" value="Save Comment">
