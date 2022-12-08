665a9a1e2c53377a46adf65174358be0ef03706f
OWOjustnobody <114700917+OWOjustnobody@users.noreply.github.com>
2022-12-07 22:21:18 -0700

    Update addEmployP.php

--- publisher/addEmployP.php
+++ publisher/addEmployP.php
@@ -21,7 +21,6 @@
         $cID = convertQuotes($_POST['cID'], "SYMBOLS");
 
         if(!empty($cID)){
-          echo "$cID";
             checkTable($conn, 'EMPLOYS');
 
             $query = "INSERT INTO EMPLOYS VALUES
